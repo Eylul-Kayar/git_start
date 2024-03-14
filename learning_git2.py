8e7de268a4afd1079003c9757db1819c5d5aa9fc
Eylul Kayar <eylulkayar@Eylul-MacBook-Air-5.local>
2024-03-14 18:14:46 +0100

    in branch two Generetive_NN is added

--- learning_git2.py
+++ learning_git2.py
@@ -1,16 +1,28 @@
 # lets make some change to merge on github page
 import torch
 
-
 def A_Function(arg):
     print(arg)
 
 def Tensor_Gen(*args):
     return torch.rand(args[0],args[1],args[2])
 
+class Generetive_NN:
+    def __init__(self, type = "GANs"):
+        self.type = type
+        self.encoder = None
+        self.decoder = None
+
+    def forward(self,image):
+        return
+
+
 if __name__ == "__main__":
 
     A_Function("it is just a function")
+    Image_generator = Generetive_NN()
+
+
 
 
 
