392ef8f13fd8bb46d8407206465bff77c4b75682
Eylul Kayar <eylulkayar@Eylul-MacBook-Air-5.local>
2024-03-14 16:05:44 +0100

    Tensor_Gen is added

--- learning_git2.py
+++ learning_git2.py
@@ -1,8 +1,12 @@
 # lets make some change to merge on github page
+import torch
 
 
 def A_Function(arg):
     print(arg)
+
+def Tensor_Gen(*args):
+    return torch.rand(args[0],args[1],args[2])
 
 if __name__ == "__main__":
 
