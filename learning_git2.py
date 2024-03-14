8ab536a9626a47cb6d70b28ecc74afe3ab6679b9
Eylul Kayar <eylulkayar@Eylul-MacBook-Air-5.local>
2024-03-14 15:48:26 +0100

    A_Function is added as a change to merge in github

--- learning_git2.py
+++ learning_git2.py
@@ -1,1 +1,12 @@
-print("change in branch")
+# lets make some change to merge on github page
+
+
+def A_Function(arg):
+    print(arg)
+
+if __name__ == "__main__":
+
+    A_Function("it is just a function")
+
+
+
